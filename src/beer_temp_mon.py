c1fa87943786df6fa6c84fc956c2af8b35e75db3
Tommy LeBlanc <tlcommodore@gmail.com>
2017-03-03 07:46:17 -0600

    Use requests package instead of urllib2

--- src/beer_temp_mon.py
+++ src/beer_temp_mon.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime as dt
 import Adafruit_MCP9808.MCP9808 as MCP9808
-import urllib2
+import requests
 import json
 
 
@@ -27,11 +27,17 @@ def returnHost():
 def local_weather(loc):
     key = "a7c0eba6c07ac8d3bc2f81535bcf8592"
     url = "https://api.darksky.net/forecast/{0}/{1:.4f},{2:.4f}?exclude=[minutely,hourly,daily,alerts,flags]".format(key, loc[0], loc[1])
+    headers = {'Content-Type': 'application/json'}
 
-    f = urllib2.urlopen(url)
-    json_string = f.read()
-    parsed_json = json.loads(json_string)
-    local_temp = parsed_json['currently']['temperature']
+    try:
+        resp = requests.get(url, headers=headers)
+
+    except Exception:
+        print('Unspecified error. Please check DarkSky availability.')
+        local_temp = 0
+    else:
+        response_msg = json.loads(resp.text)
+        local_temp = response_msg['currently']['temperature']
 
     return local_temp
 
@@ -39,8 +45,8 @@ def local_weather(loc):
 def read_temp(loc):
 
     room_temp = temp_sensor.readTempC()
-    weather_temp = 65.00        # TEMPORARY!!!
-    # weather_temp = local_weather(loc)
+    weather_temp = local_weather(loc)
+    # weather_temp = 65.09
     read_time = dt.utcnow()
 
     return room_temp, weather_temp, read_time
@@ -49,23 +55,31 @@ def read_temp(loc):
 def post_temp(loc):
 
     url = returnHost()
-    req = urllib2.Request(url)
-    req.add_header('Content-Type', 'application/json')
 
     while True:
         r_temp, w_temp, r_time = read_temp(loc)
-        res = urllib2.urlopen(req, json.dumps([c_to_f(r_temp), w_temp]))
-        status = res.read()
+        payload = {"room": c_to_f(r_temp), "weather": w_temp}
+        headers = {'Content-Type': 'application/json'}
 
-        print('Room: {0:.3f}*F - Weather: {1:.3f}*F - Time: {2} - Status: {3}'.format(c_to_f(r_temp), w_temp, r_time, 'Ok!'))
+        try:
+            r = requests.post(url, headers=headers, data=json.dumps(payload))
+        except requests.exceptions.ConnectionError:
+            print('Connection refused. Please check that service is running.')
+            return False
+        except Exception:
+            print('Unspecified error.')
+            return False
+        else:
+            response_msg = json.loads(r.text)
 
-        time.sleep(interval)
+            print('Room: {0:.3f}*F - Weather: {1:.3f}*F - Time: {2} - Status: {3}'.format(c_to_f(r_temp), w_temp, r_time, response_msg['message']))
+            time.sleep(interval)
 
 
 # ------------- Main -------------
 # Loop printing measurements every x minutes
 location = [36.1388, -86.8426]    # Location
-interval = 10                    # Rerading frequency, in seconds
+interval = 300                    # Rerading frequency, in seconds
 
 print('Press Ctrl+C to quit.')
 post_temp(location)
