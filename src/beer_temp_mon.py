2d5a5134d1ab0e74ef5c6b2bed7e76c0181034b6
Tommy Le Blanc <tlcommodore@gmail.com>
2017-04-08 14:52:26 -0500

    Add logging for monitor. More refactor/configuration needed.

--- src/beer_temp_mon.py
+++ src/beer_temp_mon.py
@@ -1,9 +1,35 @@
 import time
+import logging
 from datetime import datetime as dt
 import Adafruit_MCP9808.MCP9808 as MCP9808
 import requests
 import json
 
+
+# ************************** LOGGING **************************
+logger = logging.getLogger()
+
+streamHandler = logging.StreamHandler()
+fileHandler = logging.FileHandler('logs/error.log', mode='a')
+formatter = logging.Formatter(
+                        '%(asctime)s %(name)-2s %(levelname)-5s %(message)s')
+streamHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+
+logger.addHandler(streamHandler)
+logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)
+
+# ************************** CONFIGS **************************
+with open('config/config.json') as jsonConfig:
+    configData = json.load(jsonConfig)
+    host = configData['service']['host']
+    port = configData['service']['port']
+    route = configData['service']['route']
+
+    log_path = configData['logger']['path']
+    log_when = configData['logger']['when']
+    log_int = configData['logger']['interval']
 
 # ************************** SENSOR INIT **************************
 # Initialize the sensor
@@ -11,24 +37,15 @@ temp_sensor = MCP9808.MCP9808()
 temp_sensor.begin()
 
 
-# ************************** CONSTANT **************************
+# ************************** CONSTANTS **************************
 location = [36.1388, -86.8426]    # Location
 interval = 300                    # Rerading frequency, in seconds
-
 
 # ************************** FUNCTIONS **************************
 
 # Convert celcius to fahrenheit
 def c_to_f(c):
     return c * 9.0 / 5.0 + 32.0
-
-
-# Return external service url/port
-def returnHost():
-    with open('config/config.json') as jsonConfig:
-        configData = json.load(jsonConfig)
-
-    return configData['service']['host'] + ':' + configData['service']['port'] + configData['service']['route']
 
 
 # Retrieve local weather information
@@ -39,9 +56,8 @@ def local_weather(loc):
 
     try:
         resp = requests.get(url, headers=headers)
-
-    except Exception:
-        print('Unspecified error. Please check DarkSky availability.')
+    except Exception as e:
+        logger.error('Unspecified error. Please check DarkSky availability.', e)
         local_temp = 0
     else:
         response_msg = json.loads(resp.text)
@@ -60,8 +76,8 @@ def read_temp(loc):
     return room_temp, weather_temp, read_time
 
 
-def post_temp(loc, time_int):
-    url = returnHost()
+def post_temps(loc, time_int):
+    url = host + ':' + port + route
 
     while True:
         r_temp, w_temp, r_time = read_temp(loc)
@@ -71,24 +87,24 @@ def post_temp(loc, time_int):
         try:
             r = requests.post(url, headers=headers, data=json.dumps(payload))
         except requests.exceptions.ConnectionError:
-            print('Connection refused. Please check that service is running.')
+            logger.error('Connection refused. Please check that service is running.')
             return False
-        except Exception:
-            print('Unspecified error.')
+        except Exception as e:
+            logger.error('Unspecified error.', e)
             return False
         else:
             response_msg = json.loads(r.text)
 
-            print('Room: {0:.3f}*F - Weather: {1:.3f}*F - Time: {2} - Status: {3}'.format(c_to_f(r_temp), w_temp, r_time, response_msg['message']))
+            logger.info('Room: {0:.3f}*F - Weather: {1:.3f}*F - Time: {2} - Status: {3}'.format(c_to_f(r_temp), w_temp, r_time, response_msg['message']))
             time.sleep(time_int)
 
 
 def main(loc, time_int):
-    # Loop printing measurements every x minutes
-
     print('Press Ctrl+C to quit.')
 
-    post_temp(loc, time_int)
+    # Read and post tempetatures to API evey time_int
+    post_temps(loc, time_int)
+
 
 if __name__ == "__main__":
     main(location, interval)
